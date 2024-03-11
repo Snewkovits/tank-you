599bbf163cb0cf62fedf9ec3585a78e753f1f58c
Snewkovits <adamhunxd@gmail.com>
2024-03-11 18:34:20 +0100

    a

--- refuelling/views.py
+++ refuelling/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Refuel
+from .serializers import RefuelSerializer
 
 # Create your views here.
+
+def getRefuels(request):
+    if request.User is not None:
+        refuels = Refuel.objects.filter(user=request.User)
+        serializer = RefuelSerializer(refuels, many=True)
+        return Response(serializer)
+    return Response({'message': 'User is undefined.'})
